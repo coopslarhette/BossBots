844c2b3e81dcd1727e7c1950e9ee4b2dad7706be
stonemao9 <stonemao9@gmail.com>
2016-11-14 16:05:55 -0800

    Autonomous.java test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -35,6 +35,8 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
@@ -58,36 +60,38 @@ public class Autonomous extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     // DcMotor leftMotor = null;
     // DcMotor rightMotor = null;
-
+    private DcMotor motor1;
+    private DcMotor motor2;
+    private DcMotor motor3;
+    private DcMotor motor4;
     @Override
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
+        motor1 = hardwareMap.dcMotor.get("motor1");
+        motor1.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        /* eg: Initialize the hardware variables. Note that the strings used here as parameters
-         * to 'get' must correspond to the names assigned during the robot configuration
-         * step (using the FTC Robot Controller app on the phone).
-         */
-        // leftMotor  = hardwareMap.dcMotor.get("left_drive");
-        // rightMotor = hardwareMap.dcMotor.get("right_drive");
+        motor2 = hardwareMap.dcMotor.get("motor2");
 
-        // eg: Set the drive motor directions:
-        // "Reverse" the motor that runs backwards when connected directly to the battery
-        // leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
-        // rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+        motor3 = hardwareMap.dcMotor.get("motor3");
 
-        // Wait for the game to start (driver presses PLAY)
+        motor4 = hardwareMap.dcMotor.get("motor4");
+        motor4.setDirection(DcMotorSimple.Direction.REVERSE);
         waitForStart();
         runtime.reset();
 
         // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.update();
+        motor1.setPower(1);
+        motor4.setPower(1);
+        motor3.setPower(1);
+        motor2.setPower(1);
+        sleep(1000);
 
-            // eg: Run wheels in tank mode (note: The joystick goes negative when pushed forwards)
-            // leftMotor.setPower(-gamepad1.left_stick_y);
-            // rightMotor.setPower(-gamepad1.right_stick_y);
-        }
+        motor1.setPower(-1);
+        motor4.setPower(-1);
+        motor3.setPower(-1);
+        motor2.setPower(-1);
+        sleep(1000);
+
     }
 }
